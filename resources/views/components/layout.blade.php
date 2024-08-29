78e966e59f030178c2bc111d7ee300b4131c3b9d
Swam Htet Aung <swamhtetaungg@gmail.com>
2024-08-29 23:39:26 +0700

    Initial setup for views

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+  </head>
+  <body class="p-8 bg-black text-white">
+  </body>
+</html>
